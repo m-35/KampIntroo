a89adb22b923a5e00ac309c1fdc3fa802f39fecf
miray kozlu <mikoz1@live.com>
2021-01-20 23:51:05 +0300

    20 ocak ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+        //classı new ler isem constructor otomatik çalışıyor.
+
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            //tempArray == geçici dizi
+            T[] tempArray = items;
+            items = new T[items.Length+1]; //dizinin eleman sayısını verir.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet ( karenin altında üç nokta varsa tab tab yaptığımız)
+    class Product 
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; } //referans anahtarı
+        public string ProductName { get; set; } 
+        public double UnitPrace { get; set; } //Ürünün birim fiyatı
+        public int UnitsInStock { get; set; } //Stok Adedi
+        //bu tip classlarda sadece özellik olur.
+        //CRUD == CREATE READ UPDATE DELETE
+
+    }
+}
